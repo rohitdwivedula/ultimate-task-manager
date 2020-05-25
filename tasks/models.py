2772eea2df04b193aef9c72628b3731d1769427c
rohitdwivedula@gmail.com <rohitdwivedula@gmail.com>
2020-05-25 16:02:09 +0530

    Update models.py

--- tasks/models.py
+++ tasks/models.py
@@ -3,10 +3,12 @@ from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.utils.timezone import now
+from django_enumfield import enum
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(default=now)
+    bio = models.TextField(max_length=100)
 
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
@@ -22,8 +24,23 @@ class Label(models.Model):
 	name = models.TextField(max_length=24)
 	description = models.TextField(max_length=150, blank=True, default='') 
 	created_at = models.DateTimeField(default=now)
-
+	
 class Task(models.Model):
 	name = models.TextField(max_length = 100)
 	created_at = models.DateTimeField(default=now)
 	due_on = models.DateTimeField()
+	status = models.BooleanField()
+
+	class TaskStatus(models.TextChoices):
+        NEW = 'N' , _('New Task')
+        IN_PROGRESS = 'IP', _('In Progress')
+        COMPLETED = 'C', _('Completed')
+    status = models.CharField(
+        max_length=3,
+        choices=TaskStatus.choices,
+        default=TaskStatus.NEW,
+    )
+
+class TaskLabel(models.Model):
+	task = models.ForeignKey(Task)
+	label = models.ForeignKey(Label)
